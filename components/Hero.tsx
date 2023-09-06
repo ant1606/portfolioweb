cb54793743415339ae82f7697ca86762807d9c9a
Anthony <anthony.tp1606@gmail.com>
2023-09-06 17:52:00 -0500

    Diseñándo hero para dimension tablet

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,32 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   //TODO Agregar imagen en Hero
   return (
-    <section className='pt-5 min-w-full flex flex-col gap-6 items-center'>
-      <figure>
+    <section
+      className='pt-5 sm:pt-14 min-w-full flex flex-col gap-10 items-center 
+                sm:grid sm:grid-cols-2 '>
+      <figure className='flex justify-center' >
+        {/* <Image width={200} height={200} src="https://picsum.photos/200" alt="mi foto" /> */}
         <img src="https://picsum.photos/200" alt="mi foto" />
       </figure>
-      <h1 className='font-black text-[2.35rem] sm:text-[2.5rem] text-center leading-tight'>Hola, soy
+      <h1 className='font-black text-[2.35rem] sm:text-[2.5rem] text-center leading-tight sm:order-first sm:col-span-2'>Hola, soy
         <br className='sm:hidden' />
         <strong className='text-web-blue'> Anthony Tasayco</strong>
       </h1>
-      <h2 className='font-bold text-[1.5rem] text-center uppercase leading-8'>Desarrollador frontend & backend</h2>
-      <button
-        className='font-bold text-[1.5rem] text-white bg-web-blue rounded-tl-3xl rounded-br-3xl py-2.5 min-w-full 
-        hover:bg-web-light-blue'>
-        Contáctame
-      </button>
+      <div className='flex flex-col gap-10 justify-between items-center sm:h-full'>
+        <h2
+          className='font-bold text-[1.5rem] text-center uppercase leading-8 
+        grow flex items-end'>
+          Desarrollador frontend & backend
+        </h2>
+        <button
+          className='font-bold text-[1.5rem] text-white bg-web-blue rounded-tl-3xl rounded-br-3xl py-2.5 min-w-full 
+        hover:bg-web-light-blue '>
+          Contáctame
+        </button>
+      </div>
     </section>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,9 +14,9 @@ const Navbar = () => {
     <>
       <nav className='bg-web-blue h-15 sticky top-0 z-10'>
         <ul className='flex justify-end items-center text-web-white sm:justify-between sm:text-center'>
-          <li className='hidden sm:block sm:grow font-bold text-3xl px-5 py-3 hover:bg-web-light-blue'>Inicio</li>
-          <li className='hidden sm:block sm:grow font-bold text-3xl px-5 py-3 hover:bg-web-light-blue'>Portafolio</li>
-          <li className='hidden sm:block sm:grow font-bold text-3xl px-5 py-3 hover:bg-web-light-blue'>Contacto</li>
+          <li className='hidden font-bold text-3xl px-5 py-3 hover:bg-web-light-blue sm:block sm:grow sm:py-7 '>Inicio</li>
+          <li className='hidden font-bold text-3xl px-5 py-3 hover:bg-web-light-blue sm:block sm:grow sm:py-7 '>Portafolio</li>
+          <li className='hidden font-bold text-3xl px-5 py-3 hover:bg-web-light-blue sm:block sm:grow sm:py-7 '>Contacto</li>
           <li className='sm:hidden px-5' onClick={handleClickMobileMenu}>
             <IconContext.Provider value={{ size: '3.5em', color: '#FAFAFA' }}>
               <BiMenuAltRight />
