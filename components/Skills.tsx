de4145dad9be9b60bd2021d2da73a11cd411e7c0
Anthony <anthony.tp1606@gmail.com>
2023-09-17 21:33:52 -0500

    Modificando skills: Cambiando a cards las visualizaciones de los iconos de las skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -9,118 +9,147 @@ import typescriptLogo from '@/public/typescript_logo.svg';
 import phpLogo from '@/public/php_logo.png';
 import laravelLogo from '@/public/laravel_logo.png';
 import mysqlLogo from '@/public/mysql_logo.png';
-import sqlserverLogo from '@/public/sqlserver_logo.png';
+import sqlserverLogo from '@/public/sqlserver_logo.svg';
 import phpunitLogo from '@/public/phpunit_logo.png';
 import postmanLogo from '@/public/postman_logo.png';
 import reactTestingLibraryLogo from '@/public/reacttestinglibrary_logo.png';
 import digitalOceanLogo from '@/public/digitalocean_logo.png';
 
 import SkillCarrousel from './SkillCarrousel';
+import SkillsCard from './SkillsCard';
 
 const Skills = () => {
   const logosFrontEnd = [{
     id: 'css',
     logo: cssLogo,
     alt: 'CSS Logo',
+    name: "css"
   },
   {
     id: 'javascript',
     logo: javascriptLogo,
     alt: 'Javascript Logo',
+    name: "javascript"
   },
   {
     id: 'html',
     logo: htmlLogo,
     alt: 'HTML Logo',
+    name: "html"
   },
   {
     id: 'react',
     logo: reactLogo,
     alt: 'React Logo',
+    name: "react"
   },
   {
     id: 'typescript',
     logo: typescriptLogo,
     alt: 'Typescript Logo',
+    name: "typescript"
   }];
 
   const logosBackEnd = [{
     id: 'php',
     logo: phpLogo,
     alt: 'Php Logo',
+    name: "php"
   },
   {
     id: 'laravel',
     logo: laravelLogo,
     alt: 'Laravel Logo',
+    name: "laravel"
   }];
 
   const logosDatabase = [{
     id: 'mysql',
     logo: mysqlLogo,
     alt: 'MySql Logo',
+    name: "mysql"
   },
   {
     id: 'sqlserver',
     logo: sqlserverLogo,
     alt: 'SQL Server Logo',
+    name: "sql server"
   }];
 
   const logosTesting = [{
     id: 'phpunit',
     logo: phpunitLogo,
     alt: 'Php Unit Logo',
+    name: "php unit"
   },
   {
     id: 'postman',
     logo: postmanLogo,
     alt: 'Postman Logo',
+    name: "postman"
   },
   {
     id: 'reacttestinglibrary',
     logo: reactTestingLibraryLogo,
     alt: 'React Testing Library Logo',
+    name: "react testing library"
   }];
 
   const logosServer = [{
     id: 'digitalocean',
     logo: digitalOceanLogo,
     alt: 'Digital Ocean Logo',
+    name: "digital ocean"
   }];
 
 
 
   return (
-    <section className='uppercase text-[1.5rem] font-bold flex flex-col items-center mt-16 min-w-full'>
-      <h2 className='normal-case font-black text-[2.35rem]'>Mis Habilidades</h2>
-      <article className="flex flex-col  min-w-full items-center overflow-hidden">
+    <section className='uppercase text-[1.5rem] font-bold flex flex-col items-center mt-16 min-w-full gap-14'>
+      <h2 className='normal-case font-black text-[2.35rem] text-center'>Mis Habilidades</h2>
+      <article className="flex flex-col min-w-full items-center overflow-hidden">
         <h3>front-end</h3>
-        <div className='w-screen h-56'>
-          <SkillCarrousel arrayLogos={logosFrontEnd} />
+        <div className='w-full flex flex-col gap-8'>
+          {logosFrontEnd.map(logo =>
+            <SkillsCard key={logo.id} alt={logo.alt} image={logo.logo} name={logo.name} />
+          )}
+          {/* <SkillCarrousel arrayLogos={logosFrontEnd} /> */}
         </div>
       </article>
-      <article>
+      <article className="flex flex-col  min-w-full items-center overflow-hidden">
         <h3>back-end</h3>
-        <div className='w-screen h-56'>
-          <SkillCarrousel arrayLogos={logosBackEnd} />
+        <div className='w-full flex flex-col gap-8'>
+          {logosBackEnd.map(logo =>
+            <SkillsCard key={logo.id} alt={logo.alt} image={logo.logo} name={logo.name} />
+          )}
+          {/* <SkillCarrousel arrayLogos={logosBackEnd} /> */}
         </div>
       </article>
-      <article>
-        <h3>base de datos</h3>
-        <div className='w-screen h-56'>
-          <SkillCarrousel arrayLogos={logosDatabase} />
+      <article className="flex flex-col  min-w-full items-center overflow-hidden">
+        <h3>bases de datos</h3>
+        <div className='w-full flex flex-col gap-8'>
+          {logosDatabase.map(logo =>
+            <SkillsCard key={logo.id} alt={logo.alt} image={logo.logo} name={logo.name} />
+          )}
+          {/* <SkillCarrousel arrayLogos={logosDatabase} /> */}
         </div>
       </article>
-      <article>
+      <article className="flex flex-col  min-w-full items-center overflow-hidden">
         <h3>Testing</h3>
-        <div className='w-screen h-56'>
-          <SkillCarrousel arrayLogos={logosTesting} />
+        <div className='w-full flex flex-col gap-8'>
+          {logosTesting.map(logo =>
+            <SkillsCard key={logo.id} alt={logo.alt} image={logo.logo} name={logo.name} />
+          )}
+          {/* <SkillCarrousel arrayLogos={logosTesting} /> */}
         </div>
       </article>
-      <article>
+      <article className="flex flex-col  min-w-full items-center overflow-hidden">
         <h3>Servidores</h3>
-        <div className='w-screen h-56'>
-          <SkillCarrousel arrayLogos={logosServer} />
+        <div className='w-full'>
+          {logosServer.map(logo =>
+            <SkillsCard key={logo.id} alt={logo.alt} image={logo.logo} name={logo.name} />
+          )}
+          {/* <SkillCarrousel arrayLogos={logosServer} /> */}
         </div>
       </article>
     </section>
---
+++ components/SkillsCard.tsx
@@ -0,0 +1,22 @@
+import Image, { StaticImageData } from 'next/image'
+import React from 'react'
+
+interface Props {
+  image: StaticImageData;
+  alt: string;
+  name: string
+}
+const SkillsCard: React.FC<Props> = ({ image, alt, name }) => {
+  return (
+    <div className='border-2 pt-4 border-web-white rounded-xl overflow-hidden  flex flex-col gap-3'>
+      <div className='relative h-36'>
+        <Image alt={alt} src={image} fill={true} className='object-scale-down' />
+      </div>
+      <div className="bg-web-blue text-web-white text-center h-10">
+        <p>{name}</p>
+      </div>
+    </div>
+  )
+}
+
+export default SkillsCard
