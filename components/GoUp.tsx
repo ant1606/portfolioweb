2900ed824e09889ed6d3dfe4f9cb797778b41892
Anthony <anthony.tp1606@gmail.com>
2023-09-23 16:17:55 -0500

    fix ScrollUp: Corrigiendo comportamiento de boton GoUp

--- components/GoUp.tsx
+++ components/GoUp.tsx
@@ -5,15 +5,20 @@ import { IconContext } from 'react-icons';
 import { FaArrowUp } from "react-icons/fa6";
 
 const GoUp = () => {
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
   return (
     <div
       className='bg-web-blue rounded-full text-web-white fixed p-2 right-2 bottom-2 cursor-pointer border-2 border-web-white z-0
-      hover:bg-web-white hover:text-web-blue hover:border-web-blue  transition-colors ease-in-out'>
-      <Link href="#home">
-        <IconContext.Provider value={{ size: '1.75em' }}>
-          <FaArrowUp />
-        </IconContext.Provider>
-      </Link>
+      hover:bg-web-white hover:text-web-blue hover:border-web-blue  transition-colors ease-in-out'
+      onClick={scrollUp}>
+      <IconContext.Provider value={{ size: '1.75em' }}>
+        <FaArrowUp />
+      </IconContext.Provider>
     </div>
   )
 }
