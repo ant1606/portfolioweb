5fdc815bcaf2e31abf51509c4f4c038400445c66
Anthony <anthony.tp1606@gmail.com>
2023-09-25 14:32:55 -0500

    feature Icon y HeroImage: Agregando favicon e imagen de Seccion Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
 import Typography from './Typography'
-
+import heroImage from '@/public/heroImage.svg'
 const Hero = () => {
   //TODO Agregar imagen en Hero
   return (
     <section id="home"
       className='pt-5 sm:pt-14 min-w-full flex flex-col gap-10 items-center 
                 sm:grid sm:grid-cols-2 md:flex md:gap-12'>
-      <figure className='flex justify-center' >
-        {/* <Image width={200} height={200} src="https://picsum.photos/200" alt="mi foto" /> */}
-        <img src="https://picsum.photos/200" alt="mi foto" />
-      </figure>
+      <div className='relative w-full min-h-[200px]' >
+        <Image fill={true} src={heroImage} alt="heroImage" className='object-scale-down' />
+        {/* <a href="https://www.freepik.es/vector-gratis/ilustracion-concepto-codigo-binario_15581993.htm#query=coder&position=0&from_view=search&track=sph"></a> */}
+        {/* <img src="https://picsum.photos/200" alt="mi foto" /> */}
+      </div>
       <Typography variant="h1" className="text-web-dark sm:order-first sm:col-span-2 md:-order-none  ">
         Hola, soy <br className='sm:hidden' /> <Typography variant="strong" className="text-web-blue">Anthony Tasayco Pachas</Typography>
       </Typography>
